a7ede85bcf85c19e0224ddc3bded7219ce6605db
Klaudia Stopczynska <k.stopczynska@gmail.com>
2022-11-24 21:36:33 +0100

    Fetching countries

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CountryItem from "./CountryItem";
 import classes from "./CountriesList.module.css";
 
-const countries = [
-  { name: "Poland", population: 38000000, region: "Europe", capital: "Warsaw" },
-  {
-    name: "United States",
-    population: 323947000,
-    region: "America",
-    capital: "Washington",
-  },
-];
-
 const CountriesList = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [countries, setCountries] = useState([]);
+
+  async function fetchCountries() {
+    setIsLoading(true);
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
+    const countriesToRender = [];
+    for (const country of data) {
+      countriesToRender.push({
+        flag: country.flags.png,
+        name: country.name.official,
+        population: country.population,
+        region: country.region,
+        capital: country.capital,
+      });
+      setCountries(countriesToRender);
+      console.log(countriesToRender);
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
   return (
     <ul className={classes.list}>
-      {countries.map((country) => (
-        <CountryItem
-          key={country.name}
-          name={country.name}
-          population={country.population}
-          region={country.region}
-          capital={country.capital}
-        ></CountryItem>
-      ))}
+      {!isLoading &&
+        countries.map((country) => (
+          <CountryItem
+            flag={country.flag}
+            key={country.name}
+            name={country.name}
+            population={country.population}
+            region={country.region}
+            capital={country.capital}
+          ></CountryItem>
+        ))}
     </ul>
   );
 };
