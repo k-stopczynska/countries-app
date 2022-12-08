e765fe8d8d4e4ac406de35d491fcd170d16fe32e
Klaudia Stopczynska <k.stopczynska@gmail.com>
2022-12-08 19:36:57 +0100

    styling custom select

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun } from '@fortawesome/free-regular-svg-icons';
 import { faMoon } from '@fortawesome/free-regular-svg-icons';
+import { Link } from 'react-router-dom';
 import ThemeContext from '../contexts/theme-context';
 import classes from './Header.module.css';
 
@@ -12,7 +13,7 @@ const Header = () => {
 return (
     <React.Fragment>
         <header className={classes.header}>
-            <h1 className={classes.main__heading}>Where in the world?</h1>
+            <Link to='/'><h1 className={classes.main__heading}>Where in the world?</h1></Link>
             <button onClick={themeCtx.onChange} className={`${themeCtx.lightMode ? classes.lightMode : ''}`}>{themeCtx.lightMode ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}{themeCtx.lightMode ? 'Dark Mode' : 'Light Mode'}</button>
         </header>
     </React.Fragment>
